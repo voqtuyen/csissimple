f5db20e30ea09838cc76d495ceb32a63722711e3
Đồng Quốc Tranh <tranhdq@d-soft.com.vn>
2021-04-27 09:00:12 +0700

    Update Makefile, Add requirements.txt

--- tests/solution/person/hello_john.py
+++ tests/solution/person/hello_john.py
@@ -1,25 +1,23 @@
 import os
 
-
 print('Hello John')
 for i in range(213):
     print(i)
 
     print('you are')
 
-
-    if i==  10  :
+    if i == 10:
         print('Ok')
-class Hello:
-    
 
+
+class Hello:
     def __init__(self):
         print('Init')
-    def count(self, first_one,           cat, 
-        cow, 
-            butterfly):
+
+    def count(self, first_one, cat, cow, butterfly):
 
         pass
+
 
 def print_out(string):
 
